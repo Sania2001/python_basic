0bd8d3d0b6755ae3f91dfda5f0b77c11c80ec028
Sania2001 <saniagshinde@gmail.com>
2022-03-15 14:53:15 +0530

    Sample

---
+++ main.py
@@ -0,0 +1,20 @@
+# str1 = input("Enter")
+# unique = set(str1)
+# print("Unique words", unique)
+
+# A = input("Enter a sentence")
+# x = "#".join(A)
+# print(x)
+
+# vowels = 'aeiouAEIOU'
+# Str2 = input("Enter a sentence")
+# count = {}.fromkeys(vowels,0)
+#
+# for char in Str2:
+#     if char in count:
+#         count[char] +1
+# print(count)
+
+# str3 = input("Enter a string")
+# print(*[i for i in str3.split() if len(i) == max([len(k) for k in str3.split()])])
+# print(*[i for i in str3.split() if len(i) == min([len(k) for k in str3.split()])])
