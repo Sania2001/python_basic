7e2692ad056867aaed0bf121abff75b8a451a417
Sania2001 <saniagshinde@gmail.com>
2022-03-21 12:57:38 +0530

    assi

--- venv/Assi 7.py
+++ venv/Assi 7.py
@@ -24,26 +24,25 @@
 #
 #
 #
-#
-# import sqlite3
-# conn = sqlite3.connect('client.db')
-# c = conn.cursor()
-# sqlite_query = '''CREATE TABLE client_table(customer_id INTEGER PRIMARY KEY ,
-#                  first_name TEXT NOT NULL,
-#                  last_name TEXT NOT NULL,
-#                  salary INTEGER NOT NULL )'''
-# c.execute(sqlite_query)
-# # print("Executed Successfully")
-# insert_query = '''INSERT INTO client_table
-#                          (customer_id, first_name, last_name,  salary) VALUES
-#                          (1, 'Sania', 'Shinde', 20000),
-#                          (2, 'Karan', 'Singh', 11000),
-#                          (3, 'Mohan', 'Chand', 9000),
-#                          (4, 'Omkar', 'Patil', 12000),
-#                          (5, 'Aditya', 'Kulkarni', 15000)'''
-# c.execute(insert_query)
-# conn.commit()
-# print('Data of the client table are:')
-# c.execute(''' SELECT * FROM client_table ORDER BY first_name ASC''')
-# print(c.fetchall())
-# conn.close()
+import sqlite3
+conn = sqlite3.connect('client.db')
+c = conn.cursor()
+sqlite_query = '''CREATE TABLE client_table(customer_id INTEGER PRIMARY KEY ,
+                 first_name TEXT NOT NULL,
+                 last_name TEXT NOT NULL,
+                 salary INTEGER NOT NULL )'''
+c.execute(sqlite_query)
+# print("Executed Successfully")
+insert_query = '''INSERT INTO client_table
+                         (customer_id, first_name, last_name,  salary) VALUES
+                         (1, 'Sania', 'Shinde', 20000),
+                         (2, 'Karan', 'Singh', 11000),
+                         (3, 'Mohan', 'Chand', 9000),
+                         (4, 'Omkar', 'Patil', 12000),
+                         (5, 'Aditya', 'Kulkarni', 15000)'''
+c.execute(insert_query)
+conn.commit()
+print('Data of the client table are:')
+c.execute(''' SELECT * FROM client_table ORDER BY first_name ASC''')
+print(c.fetchall())
+conn.close()
